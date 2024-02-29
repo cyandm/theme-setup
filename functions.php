d3b685f89b1df403b2b1d2819bbede871fbb59a9
elahe <elahemahmoodi81@gmail.com>
2024-02-29 16:57:49 +0330

    iconsax fixed & theme update checker files added

--- functions.php
+++ functions.php
@@ -6,3 +6,4 @@ require_once(__DIR__ . '/inc/functions/cyn-register.php');
 require_once(__DIR__ . '/inc/functions/cyn-customize.php');
 require_once(__DIR__ . '/inc/functions/cyn-acf.php');
 require_once(__DIR__ . '/inc/functions/cyn-general.php');
+require_once(__DIR__ . '/inc/functions/cyn-update-checker.php' );
